f8f22acaee003464a152323204acccd4b59f997f
Enoch Baah <ebaah2942@gmail.com>
2024-09-19 23:53:52 +0000

    Add changes to pattern_drawing.py file

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -7,10 +7,3 @@ while row < pattern_size:
     print()
     row += 1
 
-# for i in range(pattern_size):
-#     for j in range(pattern_size):
-#         if i == 0 or i == pattern_size - 1 or j == 0 or j == pattern_size - 1:
-#             print("*", end="")
-#         else:
-#             print(" ", end="")
-#     print()
