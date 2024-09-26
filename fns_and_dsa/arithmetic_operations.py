c6ff1bbf07cd29fbc42e19fe465932bd7b5723cc
Enoch Baah <ebaah2942@gmail.com>
2024-09-26 00:55:08 +0000

    New changes

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -5,19 +5,19 @@ def perform_operation():
 
     if operation == "add":
         result = num1 + num2
-        print(f"{num1},{num2}, {operation} = {result}")
+        print(f"{num1},{num2}, {operation}")
     elif operation == "subtract":
         result = num1 - num2
-        print(f"{num1},{num2}, {operation} = {result}")
+        print(f"{num1},{num2}, {operation}")
     elif operation == "multiply":
         result = num1 * num2
-        print(f"{num1},{num2}, {operation} = {result}")
+        print(f"{num1},{num2}, {operation}")
     elif operation == "divide":
         if num2 == 0:
             print("Cannot divide by zero.")
             return
         result = num1 / num2
-        print(f"{num1},{num2}, {operation} = {result}")
+        print(f"{num1},{num2}, {operation}")
     else:
         print("Invalid operation.")
         return
