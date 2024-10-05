545ffab73e58cb0735f86b41ce9b5cbe0b02463a
Enoch Baah <ebaah2942@gmail.com>
2024-10-05 13:21:56 +0000

    New changes

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -12,19 +12,19 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(0, 0), 0)  # Adding zero
         self.assertEqual(self.calc.add(-2, 3), 1)  # Mixed sign numbers
 
-     def test_subtract(self):
+     def test_subtraction(self):
         self.assertEqual(self.calc.subtract(5, 3), 2) 
         self.assertEqual(self.calc.subtract(-5, -3), -2)  
         self.assertEqual(self.calc.subtract(0, 0), 0)  
         self.assertEqual(self.calc.subtract(3, -3), 6) 
 
-     def test_multiply(self):
+     def test_multiplication(self):
         self.assertEqual(self.calc.multiply(2, 3), 6)  
         self.assertEqual(self.calc.multiply(-2, 3), -6)  
         self.assertEqual(self.calc.multiply(0, 5), 0)  
         self.assertEqual(self.calc.multiply(-2, -3), 6)  
 
-     def test_divide(self):
+     def test_division(self):
         self.assertEqual(self.calc.divide(6, 3), 2) 
         self.assertEqual(self.calc.divide(-6, 3), -2)  
         self.assertEqual(self.calc.divide(0, 1), 0)  
